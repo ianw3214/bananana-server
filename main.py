7b6b16b2d48076cb3aea5b5263dec00e67918c9f
Ian <ianw3214@gmail.com>
2020-03-26 16:09:06 -0400

    Fix environment variable

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> greeting")
 
-start_server = websockets.serve(hello, "0.0.0.0", os.getenv["PORT"]);
+start_server = websockets.serve(hello, "0.0.0.0", os.environ["PORT"]);
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
