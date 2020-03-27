a1366ae468745bb3c34c796dbb357a13ce6ec3cd
Ian <ianw3214@gmail.com>
2020-03-26 23:53:32 -0400

    Recieve client messages and print

--- main.py
+++ main.py
@@ -8,22 +8,40 @@ import os
 import datetime
 import random
 
+async def consumer(message):
+    print(message)
+
+async def producer():
+    now = datetime.datetime.utcnow().isoformat() + "Z"
+    return now
+
+async def consumer_handler(websocket, path):
+    async for message in websocket:
+        await consumer(message)
+
+async def producer_handler(websocket, path):
+    while True:
+        message = await producer()
+        await websocket.send(message)
+        await asyncio.sleep(random.random() * 5)
+
 # WebSocket server example
 async def hello(websocket, path):
     print("HELLO FUNCTION START")
     
-    while True:
-            now = datetime.datetime.utcnow().isoformat() + "Z"
-            await websocket.send(now)
-            await asyncio.sleep(random.random() * 3)
+    consumer_task = asyncio.ensure_future(consumer_handler(websocket, path))
+    producer_task = asyncio.ensure_future(producer_handler(websocket, path))
+    done, pending = await asyncio.wait(
+        [consumer_task, producer_task],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+    for task in pending:
+            task.cancel()
 
-    # name = await websocket.recv()
-    # print(f"< {name}")
-
-    # greeting = f"Hello {name}!"
-
-    # await websocket.send(greeting)
-    # print(f"> greeting")
+    # while True:
+    #         now = datetime.datetime.utcnow().isoformat() + "Z"
+    #         await websocket.send(now)
+    #         await asyncio.sleep(random.random() * 3)
 
 start_server = websockets.serve(hello, "0.0.0.0", os.environ["PORT"])
 
