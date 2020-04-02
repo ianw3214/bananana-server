cac87dd0628c3e25f31385e3ef5150448bd763d6
Ian <ianw3214@gmail.com>
2020-04-02 01:22:06 -0400

    Implement basic fishing

--- main.py
+++ main.py
@@ -4,14 +4,42 @@ import asyncio
 import websockets
 import os
 import json
+import sys
+import random
 
 # TEMPORARY
 import datetime
 import random
 
+# CLIENT PROPERTIES
+# - id: client id
+# - x: client x position
+# - y: client y position
+# - state: client state in case the client is doing something
 CLIENTS = []
 
-async def sendMessage(message):
+def getClient(target):
+    for client in CLIENTS:
+        if client["id"] == target:
+            return client
+    return None
+
+# TEMPORARY CODE FOR FISHING
+FISH = ["GOLDFISH", "COMMON CARP", "CLOWNFISH", "CATFISH", "SWORDFISH", "SALMON", "LARGE COD", "SERGIO"]
+
+def update():
+    for client in CLIENTS:
+        if client["state"] == "fishing":
+            # Randomly let the client find a fish
+            if random.randint(0, 10) == 0:
+                client["state"] = "default"
+                sendMessage({
+                    "command": "fish",
+                    "id": client["id"],
+                    "fish": FISH[random.randint(0, 7)]
+                })
+
+def sendMessage(message):
     for client in CLIENTS:
         client["messages"].append(message)
 
@@ -23,15 +51,26 @@ async def consumer(message, websocket):
         messages = []
         for client in CLIENTS:
             messages.append({"command": "create", "id": client["id"], "x": client["x"], "y": client["y"]})
-        CLIENTS.append({"id": command["id"], "socket": websocket, "messages": messages})
+        CLIENTS.append({
+            "id": command["id"], 
+            "socket": websocket, 
+            "messages": messages,
+            "state": "default"
+        })
         # Have an initial message in the client to signal it has joined
-        await sendMessage({"command": "create", "id": command["id"], "x": 320, "y": 240})
+        sendMessage({"command": "create", "id": command["id"], "x": 480, "y": 360, "state": "default"})
     if command["command"] == "move":
-        for client in CLIENTS:
-            if client["id"] == command["id"]:
-                client["x"] = command["x"]
-                client["y"] = command["y"]
-        await sendMessage({"command": "move", "id": command["id"], "x": command["x"], "y": command["y"]})
+        client = getClient(command["id"])
+        if client and client["state"] == "default":
+            client["x"] = command["x"]
+            client["y"] = command["y"]
+            sendMessage({"command": "move", "id": command["id"], "x": command["x"], "y": command["y"], "debug": "STATE: " + client["state"]})
+    if command["command"] == "interact":
+        client = getClient(command["id"])
+        if client:
+            if command["action"] == "fishing":
+                client["state"] = "fishing"
+            sendMessage({"command": "interact", "id": command["id"], "action": "fishing"})
 
 async def producer(websocket):
     for client in CLIENTS:
@@ -51,7 +90,7 @@ async def consumer_handler(websocket, path):
             target = client["id"]
             CLIENTS.remove(client)
     if target != 0:
-        await sendMessage({"command": "remove", "id": target})
+        sendMessage({"command": "remove", "id": target})
 
 async def producer_handler(websocket, path):
     while True:
@@ -60,6 +99,8 @@ async def producer_handler(websocket, path):
             await websocket.send(message)
         # SEND MESSAGES AT A 1 SECOND INTERVAL
         await asyncio.sleep(0.5)
+        # JUST LEAVE UPDATE LOOP HERE AND HOPE IT WORKS
+        update()
 
 # WebSocket server example
 async def hello(websocket, path):
@@ -70,10 +111,21 @@ async def hello(websocket, path):
         return_when=asyncio.FIRST_COMPLETED,
     )
     for task in pending:
-            task.cancel()
+        task.cancel()
 
-start_server = websockets.serve(hello, "0.0.0.0", os.environ["PORT"])
+if __name__ == "__main__":
+    address = "0.0.0.0"
+    port = 0
 
-print("SERVER STARTED")
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+    if os.getenv("ADDRESS"):
+        address = os.environ["ADDRESS"]
+    if os.getenv("PORT"):
+        port = os.environ["PORT"]
+
+    start_server = websockets.serve(hello, address, port);
+    print("SERVER STARTED")
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+    
